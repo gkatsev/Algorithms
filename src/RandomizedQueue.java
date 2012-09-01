2f6fa407af522c59fe9e337295b83cb01895b38c
Gary Katsevman <git@gkatsev.com>
2012-08-31 23:27:41 -0400

    Implementation of RandomizedQueue

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * User: Gary Katsevman
@@ -6,46 +7,86 @@ import java.util.Iterator;
  * Time: 11:47 PM
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] a;
+    private int N;
+
     public RandomizedQueue() {
-
+        a = (Item[]) new Object[2];
+        N = 0;
     }
 
-    public boolean isEmpty() {
-        return false;
+    public boolean isEmpty() { return N == 0; }
+    public int size() { return N; }
+
+    private void resize(int capacity) {
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < N; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
     }
 
-    public int size() {
-        return 0;
+    private void checkNull(Item item) {
+        if (item == null) {
+            throw new NullPointerException("We do not support empty items");
+        }
+    }
+    private void checkEmpty() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("There are no items in the RandomizedQueue");
+        }
     }
 
     public void enqueue(Item item) {
+        checkNull(item);
 
+        if (N == a.length) resize(a.length*2);
+        a[N++]  = item;
     }
 
     public Item dequeue() {
-        return null;
+        checkEmpty();
+
+        int r = StdRandom.uniform(N);
+        Item item = a[r];
+        a[r] = a[--N];
+        a[N] = null;
+        if (N > 0 && N  == a.length/4) resize(a.length/2);
+        return item;
     }
 
     public Item sample() {
-        return null;
+        checkEmpty();
+
+        return a[StdRandom.uniform(N)];
     }
 
     public Iterator<Item> iterator() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return new RandomizedQueueIterator();
     }
 
     private class RandomizedQueueIterator implements Iterator<Item> {
 
-        public RandomizedQueueIterator() {
+        int[] order;
+        int current;
 
+        public RandomizedQueueIterator() {
+            order = new int[N];
+            for (int i = 0; i < N; i++) {
+                int r = StdRandom.uniform(i+1);
+                order[i] = order[r];
+                order[r] = i;
+            }
+            current = 0;
         }
 
         public boolean hasNext() {
-            return false;  //To change body of implemented methods use File | Settings | File Templates.
+            return current != order.length;
         }
 
         public Item next() {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
+            if (current == order.length) { throw new NoSuchElementException("We got to the end and found nothing."); }
+            return a[order[current++]];
         }
 
         public void remove() {
