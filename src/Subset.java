e07ee21042535138522ccef4d12ece06f3a18347
Gary Katsevman <git@gkatsev.com>
2012-08-31 23:50:06 -0400

    subset implementation

--- src/Subset.java
+++ src/Subset.java
@@ -5,5 +5,11 @@
  */
 public class Subset {
     public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        String[] inputs = StdIn.readStrings();
+        StdRandom.shuffle(inputs);
+        for (int i = 0; i < k; i++) {
+            StdOut.println(inputs[i]);
+        }
     }
 }
