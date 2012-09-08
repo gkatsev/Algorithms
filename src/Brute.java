fefe852e65ff4720a0a347be9bba59c1d13dc036
Gary Katsevman <git@gkatsev.com>
2012-09-08 01:02:31 -0400

    add initial Brute implementation

---
+++ src/Brute.java
@@ -0,0 +1,68 @@
+/**
+ * User: Gary Katsevman
+ * Date: 9/8/12
+ * Time: 12:40 AM
+ */
+public class Brute {
+    private static boolean collinear(Point a, Point b, Point c) {
+        double sAB = a.slopeTo(b);
+        double sAC = a.slopeTo(c);
+        double sBC = b.slopeTo(c);
+
+        return sAB == sAC && sAB == sBC;
+    }
+
+    private static void printPoints(Point a, Point b, Point c, Point d) {
+        StdOut.print(a.toString());
+        StdOut.print(" -> ");
+        StdOut.print(b.toString());
+        StdOut.print(" -> ");
+        StdOut.print(c.toString());
+        StdOut.print(" -> ");
+        StdOut.print(d.toString());
+        StdOut.println();
+    }
+
+    private static void drawPoints(Point a, Point b, Point c, Point d) {
+        a.drawTo(d);
+    }
+
+    private static void iterate(Point[] points) {
+        int n = points.length;
+        for (int i = 0; i < n; i++) {
+            Point a = points[i];
+            for (int j = 0; j < n; j++) {
+                Point b = points[j];
+                for (int k = 0; k < n; k++) {
+                    Point c = points[k];
+                    for (int l = 0; l < n; l++) {
+                        Point d = points[l];
+                        a.draw();
+                        b.draw();
+                        c.draw();
+                        d.draw();
+                        if (collinear(a, b, c)) {
+                            if (collinear(b, c, d)) {
+                                printPoints(a, b, c, d);
+                                drawPoints(a, b, c, d);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        iterate(points);
+    }
+}
